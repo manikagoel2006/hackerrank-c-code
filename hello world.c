85fb7e0788f4feccac5e7d669191932d0669d063
manikagoel2006 <56432373+manikagoel2006@users.noreply.github.com>
2020-06-15 17:52:06 +0530

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -10,16 +10,4 @@ int main ()
     printf("Hello, World!\n%s", s);
     
     return 0;
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-int main ()
-{
-    char s[100];
-    fgets(s, sizeof(s), stdin);
-    printf("Hello, World!\n%s", s);
-    
-    return 0;
 }
